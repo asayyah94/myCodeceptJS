d8c73eb4b2a4bc13d91afea0799687edfbccb601
Amirhossein Sayyah <asayyah94@gmail.com>
2020-07-06 17:37:49 -0700

    Update submit_test.js
    Adding contact page submit validation using assertion.

--- submit_test.js
+++ submit_test.js
@@ -1,5 +1,11 @@
 Feature('submit');
 
-Scenario('test something', (I) => {
-	
+Scenario('check submission on contact page', (I) => {
+	I.amOnPage('/contact');
+	I.fillField('Name', 'Amirhossein');
+	I.fillField('Email', 'asayyah1994@gmail.com');
+	I.fillField('Website', 'https://github.com/asayyah94');
+	I.fillField('Date', 'July 4, 2020');
+	I.click('Submit');
+	I.see('Message Sent');
 });
